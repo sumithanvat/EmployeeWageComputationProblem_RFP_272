d740dcc945c432b1e2d485e1624364be9e384511
sumithanvat <skhanvat@gmail.com>
2023-04-18 00:22:59 +0530

    Added:Cheking employee is present or absent

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,5 +3,16 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Wellcome to Employee Wage ");
+
+        int attendance =(int) Math.round(Math.random());
+
+        if (attendance == 1){
+            System.out.println("employee is present");
+        }
+        else {
+            System.out.println("employee is absent");
+        }
+
+
     }
 }
