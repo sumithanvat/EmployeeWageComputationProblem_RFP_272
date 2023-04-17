357c7db1ac3d897411b0c960c3962d1717ac237d
sumithanvat <skhanvat@gmail.com>
2023-04-18 00:59:50 +0530

    Added:Use Switch Case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Wellcome to Employee Wage ");
@@ -21,6 +23,24 @@ public class EmployeeWage {
         int partTimeHour = 4;
         dailyWage =partTimeHour*wagePerHour;
         System.out.println("uc3 Daily wage of part time is :- "+dailyWage);
+
+        //uc4
+        int employeeType=new Random().nextInt(2)+1;
+
+        // using switch case
+        switch (employeeType){
+            case 1:
+                dailyWage = wagePerHour * fullTimeHour;
+                System.out.println("uc4 full time wage of employee is :-"+dailyWage);
+                break;
+            case 2:
+                dailyWage = wagePerHour * partTimeHour;
+                System.out.println("uc4 part time wage of employee is :-"+dailyWage);
+                break;
+            default:
+                System.out.println("invalid employee");
+        }
+
 
 
 
